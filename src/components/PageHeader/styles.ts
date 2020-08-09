115fc2b266037e781a6e0706b7be3ceeadb0d4f5
Guilherme Gonçalves <guilhermegufes@gmail.com>
2020-08-08 22:45:02 -0300

    Finalizaçao da quinta aula

--- src/components/PageHeader/styles.ts
+++ src/components/PageHeader/styles.ts
@@ -12,6 +12,12 @@ const styles = StyleSheet.create({
         justifyContent: 'space-between'
     },
 
+    header: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between'
+    },
+
     title: {
         fontFamily: 'Archivo_700Bold',
         color: '#FFF',
@@ -22,4 +28,4 @@ const styles = StyleSheet.create({
     }
 })
 
-export default styles
+export default styles;
